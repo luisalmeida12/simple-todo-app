bd01279f6e76a294dfcf71de4f609da6d257bfba
Luis Almeida <luisdmalmeida@gmail.com>
2020-10-24 19:48:53 +0100

    Updates

---
+++ precache-manifest.42e3640fb91a62005d82753f25ec4eaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98c29aa4f5bb6fb0d99b1d399b9d7d27",
+    "url": "/luisalmeida12/simple-todo-app/index.html"
+  },
+  {
+    "revision": "f866faa628b8d7250435",
+    "url": "/luisalmeida12/simple-todo-app/static/css/main.36d00def.chunk.css"
+  },
+  {
+    "revision": "468f9c4dfa7c5064d329",
+    "url": "/luisalmeida12/simple-todo-app/static/js/2.354bc41a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/luisalmeida12/simple-todo-app/static/js/2.354bc41a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f866faa628b8d7250435",
+    "url": "/luisalmeida12/simple-todo-app/static/js/main.c8f6aee7.chunk.js"
+  },
+  {
+    "revision": "288e1d60ce178af40a12",
+    "url": "/luisalmeida12/simple-todo-app/static/js/runtime-main.8524439a.js"
+  }
+]);
